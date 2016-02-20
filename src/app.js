041cb98d260516857d4aff300bf6170bc85b75d8
oxyflour <oxyflour@hotmail.com>
2016-02-21 02:37:49 +0800

    update api proxy address

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { COLOR } from 'react-native-material-design'
 
-const SERVER_URL = 'http://u.ofr.me:8082'
+const SERVER_URL = 'http://vlr.ofr.me:8082'
 
 export function getThemePrimary() {
 	// see
